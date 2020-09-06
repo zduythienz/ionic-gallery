3433de50d9599d0ded9d353a2267214ebf05d93f
zdtz <zduythienz@gmail.com>
2020-09-06 15:21:14 +0700

    implement add new photo to fileSystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -19,6 +19,44 @@ export class PhotoService {
 
   public photos: Photo[] = [];
 
+  private async savePicture(cameraPhoto: CameraPhoto) {
+
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -27,11 +65,10 @@ export class PhotoService {
       quality: 100
     });
 
-    this.photos.unshift({
-      filepath: 'soon...',
-      webviewPath: capturedPhoto.webPath
-    });
+    const savedPhoto = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedPhoto);
   }
+
 }
 
 interface Photo {
