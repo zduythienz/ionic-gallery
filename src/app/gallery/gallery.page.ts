751d660c8eda811f35e35b2e8cde6a968eb41f4a
zdtz <zduythienz@gmail.com>
2020-09-06 16:24:33 +0700

    save to localservice

--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-gallery',
   templateUrl: 'gallery.page.html',
   styleUrls: ['gallery.page.scss']
 })
-export class GalleryPage {
+export class GalleryPage implements OnInit {
+
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+  ) { }
 
   photos = this.photoService.photos;
 
-  constructor(public photoService: PhotoService) { }
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
+
+  public async showActionSheet(photo, position) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 
 }
